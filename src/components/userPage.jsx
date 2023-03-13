c52bcf17c4207188f1906ae28329d32e905e5331
Konstantin Savelev <kost9978rus@gmail.com>
2023-03-13 23:01:27 +0300

    2023_03_13

--- src/components/userPage.jsx
+++ src/components/userPage.jsx
@@ -1,25 +1,27 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { useHistory } from "react-router-dom";
+import QualitiesList from "./qualitiesList";
 const UserPage = ({ user }) => {
-    // console.log("user2", user);
     const history = useHistory();
     const handleReturn = () => {
         history.push("/users");
     };
     return (
         <>
-            {/* <ul>
-                <li><span>Имя</span>{user.name}</li>
-            </ul> */}
-            <h1>User</h1>
+
+            <h1>{user.name}</h1>
+            <h2>{`profession: ${user.profession.name}`}</h2>
+            <QualitiesList qualities={user.qualities} />
+            <h3>{`completedMeetings: ${user.completedMeetings}`}</h3>
+            <h1>{`rate: ${user.rate}`}</h1>
             <button type="button" onClick={handleReturn}>
-                All users
+                Все пользователи
             </button>
         </>
     );
 };
 UserPage.propTypes = {
-    user: PropTypes.object
+    user: PropTypes.object.isRequired
 };
 export default UserPage;
--- src/components/users.jsx
+++ src/components/users.jsx
@@ -11,107 +11,103 @@ import { useParams } from "react-router-dom";
 
 const Users = () => {
     const { userId } = useParams();
-    // console.log("userId", userId);
-    if (userId) {
-        const [user, setUser] = useState();
-        useEffect(() => {
-            API.users.getById(userId).then((data) => setUser(data));
-        }, []);
-        // console.log("user1", user);
-        return user ? <UserPage user={user} /> : "Loading...";
-    } else {
-        const [users, setUsers] = useState();
-        useEffect(() => {
-            API.users.fetchAll().then((data) => setUsers(data));
-        }, []);
-        const handleDelete = (userId) => {
-            setUsers(users.filter((user) => user._id !== userId));
-        };
-        const hendleBookmark = (id) => {
-            setUsers((prevSate) =>
-                prevSate.map((user) =>
-                    user._id === id ? { ...user, bookmark: !user.bookmark } : user
-                )
-            );
-        };
-        const pageSize = 8;
-        const [currentPage, setCurrentPage] = useState(1);
-        const [professions, setProfessions] = useState();
-        const [selectedProf, setSelectedProf] = useState();
-        const [sortBy, setSortBy] = useState({ path: "name", order: "asc" });
-        useEffect(() => {
-            API.professions.fetchAll().then((data) => setProfessions(data));
-        }, []);
-        useEffect(() => setCurrentPage(1), [selectedProf]);
 
-        const handleProfessionSelect = (id) => {
-            setSelectedProf(id);
-        };
-        const hendlePageChange = (pageIndex) => {
-            setCurrentPage(pageIndex);
-        };
-        const handleSort = (item) => {
-            setSortBy(item);
-            setCurrentPage(1);
-        };
-        if (users) {
-            const filteredUsers = selectedProf
-                ? users.filter(
-                    (user) =>
-                        JSON.stringify(user.profession) ===
-                        JSON.stringify(selectedProf)
-                )
-                : users;
+    const [user, setUser] = useState();
+    useEffect(() => {
+        API.users.getById(userId).then((data) => setUser(data));
+    });
 
-            const itemsCount = filteredUsers.length;
-            const sortedUsers = _.orderBy(filteredUsers, [sortBy.path], [sortBy.order]);
-            const usersCrop = paginate(sortedUsers, currentPage, pageSize);
-            const clearFilter = () => {
-                setSelectedProf();
-            };
-            return (
-                <div className="d-flex">
-                    {professions && (
-                        <div className="d-flex flex-column flex-shrink-0 p-3">
-                            <GroupList
-                                items={professions}
-                                valueProperty="_id"
-                                contentProperty="name"
-                                onProfessionSelect={handleProfessionSelect}
-                                selectedProf={selectedProf}
-                            />
-                            <button
-                                className="btn btn-secondary mt-2"
-                                onClick={clearFilter}
-                            >
-                                Очистить
-                            </button>
-                        </div>
+    const [users, setUsers] = useState();
+    useEffect(() => {
+        API.users.fetchAll().then((data) => setUsers(data));
+    }, []);
+    const handleDelete = (userId) => {
+        setUsers(users.filter((user) => user._id !== userId));
+    };
+    const hendleBookmark = (id) => {
+        setUsers((prevSate) =>
+            prevSate.map((user) =>
+                user._id === id ? { ...user, bookmark: !user.bookmark } : user
+            )
+        );
+    };
+    const pageSize = 8;
+    const [currentPage, setCurrentPage] = useState(1);
+    const [professions, setProfessions] = useState();
+    const [selectedProf, setSelectedProf] = useState();
+    const [sortBy, setSortBy] = useState({ path: "name", order: "asc" });
+    useEffect(() => {
+        API.professions.fetchAll().then((data) => setProfessions(data));
+    }, []);
+    useEffect(() => setCurrentPage(1), [selectedProf]);
+
+    const handleProfessionSelect = (id) => {
+        setSelectedProf(id);
+    };
+    const hendlePageChange = (pageIndex) => {
+        setCurrentPage(pageIndex);
+    };
+    const handleSort = (item) => {
+        setSortBy(item);
+        setCurrentPage(1);
+    };
+    if (users && !userId) {
+        const filteredUsers = selectedProf
+            ? users.filter(
+                (user) =>
+                    JSON.stringify(user.profession) ===
+                    JSON.stringify(selectedProf)
+            )
+            : users;
+
+        const itemsCount = filteredUsers.length;
+        const sortedUsers = _.orderBy(filteredUsers, [sortBy.path], [sortBy.order]);
+        const usersCrop = paginate(sortedUsers, currentPage, pageSize);
+        const clearFilter = () => {
+            setSelectedProf();
+        };
+        return (
+            <div className="d-flex">
+                {professions && (
+                    <div className="d-flex flex-column flex-shrink-0 p-3">
+                        <GroupList
+                            items={professions}
+                            valueProperty="_id"
+                            contentProperty="name"
+                            onProfessionSelect={handleProfessionSelect}
+                            selectedProf={selectedProf}
+                        />
+                        <button
+                            className="btn btn-secondary mt-2"
+                            onClick={clearFilter}
+                        >
+                            Очистить
+                        </button>
+                    </div>
+                )}
+                <div className="d-flex flex-column">
+                    <Status number={itemsCount} />
+                    {users.length > 0 && (
+                        <UsersTable users={usersCrop}
+                            onSort={handleSort}
+                            selectedSort={sortBy}
+                            handleDelete={handleDelete}
+                            hendleBookmark={hendleBookmark} />
+
                     )}
-                    <div className="d-flex flex-column">
-                        <Status number={itemsCount} />
-                        {users.length > 0 && (
-                            <UsersTable users={usersCrop}
-                                onSort={handleSort}
-                                selectedSort={sortBy}
-                                handleDelete={handleDelete}
-                                hendleBookmark={hendleBookmark} />
 
-                        )}
-
-                        <div className="d-flex justify-content-center">
-                            <Pagination
-                                itemsCount={itemsCount}
-                                pageSize={pageSize}
-                                onPageChange={hendlePageChange}
-                                currentPage={currentPage}
-                            />
-                        </div>
+                    <div className="d-flex justify-content-center">
+                        <Pagination
+                            itemsCount={itemsCount}
+                            pageSize={pageSize}
+                            onPageChange={hendlePageChange}
+                            currentPage={currentPage}
+                        />
                     </div>
                 </div>
-            );
-        }
-        return "Loading...";
-    }
+            </div>
+        );
+    } else if (userId && user) { return <UserPage user={user} />; }
+    return "Loading...";
 };
 export default Users;
